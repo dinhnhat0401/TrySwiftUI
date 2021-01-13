7f866c0c0a88bc25d5fcb723bc7c15b2bca2a519
Nhat Dinh <vannhat-dinh.ae@cmic.co.jp>
2021-01-13 22:19:39 +0900

    building list and navigation

---
+++ Landmarks/Model/Landmark.swift
@@ -0,0 +1,37 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Dinh Van Nhat on 2021/01/13.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+
+    struct Coordinates: Hashable, Codable {
+      var latitude: Double
+      var longitude: Double
+    }
+}
---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Dinh Van Nhat on 2021/01/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
